3d0c759e53c6ef03307441310d23d69cdb9790a1
Tinchota <martin.belcic@gmail.com>
2020-04-20 16:19:14 -0300

    Socket server

--- main.c
+++ main.c
@@ -3,7 +3,10 @@
 #include <stdio.h>
 #include <unistd.h> //para el sleep
 #include <pthread.h>
+#include <stdlib.h> 
 #include <sys/socket.h>
+#include <netinet/in.h> 
+#include <string.h> 
 
 #define STATE_FREE          0
 #define STATE_RESERVED      1
@@ -12,6 +15,7 @@
 #define REQUEST_CONFIRM     2
 #define REQUEST_MAX         10
 #define TICKETS_TOTAL       10
+#define PORT 12345
 
 typedef struct {
     char type;
@@ -276,11 +280,69 @@ void *dbThreadFunction(void *vargp) {
 ///
 
 void *socketThreadFunction(void *vargp) {
+    int server_fd, new_socket, valread; 
+    struct sockaddr_in address; 
+    int opt = 1; 
+    int addrlen = sizeof(address); 
+    char buffer[1024] = {0}; 
+    char *hello = "Server iniciado."; 
+
+    // Creating socket file descriptor 
+    if ((server_fd = socket(AF_INET, SOCK_STREAM, 0)) == 0) 
+    { 
+        perror("socket failed"); 
+        exit(EXIT_FAILURE); 
+    } 
+
+    // Forcefully attaching socket to the port
+    if (setsockopt(server_fd, SOL_SOCKET, SO_REUSEADDR | SO_REUSEPORT, &opt, sizeof(opt))) 
+    { 
+        perror("setsockopt"); 
+        exit(EXIT_FAILURE); 
+    } 
+    address.sin_family = AF_INET; 
+    address.sin_addr.s_addr = INADDR_ANY; 
+    address.sin_port = htons( PORT ); 
+
+    // Forcefully attaching socket to the port
+    if (bind(server_fd, (struct sockaddr *)&address, sizeof(address))<0) 
+    { 
+        perror("bind failed"); 
+        exit(EXIT_FAILURE); 
+    } 
+    if (listen(server_fd, 3) < 0) 
+    { 
+        perror("listen"); 
+        exit(EXIT_FAILURE); 
+    } 
+    if ((new_socket = accept(server_fd, (struct sockaddr *)&address, (socklen_t*)&addrlen))<0) 
+    { 
+        perror("accept"); 
+        exit(EXIT_FAILURE); 
+    } 
+
+    valread = read( new_socket , buffer, 1024); 
+    printf("%s\n", buffer);
+
+    // Create Request Thread
+    // Parse JSon
+    int req_type, ticket_id;
+    if (req_type == 1){
+        createRequestThread(REQUEST_RESERVE, 0);
+    }
+    else if (req_type == 2){
+        createRequestThread(REQUEST_CONFIRM, ticket_id);
+    }
+
+    send(new_socket , hello , strlen(hello) , 0 ); 
+    printf("Hello message sent\n"); 
+       
+    /*
     sleep(1);
     createRequestThread(REQUEST_RESERVE, 0);
     sleep(1);
     createRequestThread(REQUEST_CONFIRM, 5);
-
+    */
     // TODO
 
     return NULL;
--- main_client.c
+++ main_client.c
@@ -5,7 +5,7 @@
 #include <arpa/inet.h> 
 #include <unistd.h> 
 #include <string.h> 
-#define PORT 8080 
+#define PORT 12345 
    
 int main(int argc, char const *argv[]) 
 { 
